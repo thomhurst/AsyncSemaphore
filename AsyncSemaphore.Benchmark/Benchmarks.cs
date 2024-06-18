98522ff33ccc662b655d36d6c19b64dd4390ae38
Tom Longhurst <130671542+msm-tomlonghurst@users.noreply.github.com>
2024-06-18 15:02:49 +0100

    Benchmarks

---
+++ AsyncSemaphore.Benchmark/Benchmarks.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace AsyncSemaphore.Benchmark;
+
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    private readonly SemaphoreSlim _semaphoreSlim = new(1, 1);
+    private readonly Semaphores.AsyncSemaphore _asyncSemaphore = new(1);
+
+    [Benchmark]
+    public async Task Raw_Semaphore_Slim()
+    {
+        try
+        {
+            await _semaphoreSlim.WaitAsync();
+        }
+        finally
+        {
+            _semaphoreSlim.Release();
+        }
+    }
+    
+    [Benchmark]
+    public async Task AsyncSemaphore_With_Inherited_Scope()
+    {
+        using var _ = await _asyncSemaphore.WaitAsync();
+    }
+    
+    [Benchmark]
+    public async Task AsyncSemaphore_With_Braced_Scope()
+    {
+        using (await _asyncSemaphore.WaitAsync())
+        {
+        }
+    }
+}
--- AsyncSemaphore/AsyncSemaphoreLock.cs
+++ AsyncSemaphore/AsyncSemaphoreLock.cs
@@ -1,21 +1,9 @@
-using System.Runtime.CompilerServices;
-
 namespace Semaphores;
 
-internal class AsyncSemaphoreLock(SemaphoreSlim semaphoreSlim) : IDisposable
+internal sealed class AsyncSemaphoreLock(SemaphoreSlim semaphoreSlim) : IDisposable
 {
-    private bool _isDisposed;
-
-    [MethodImpl(MethodImplOptions.Synchronized)]
     public void Dispose()
     {
-        if (_isDisposed)
-        {
-            return;
-        }
-        
-        _isDisposed = true;
-        
         semaphoreSlim.Release();
     }
 }
